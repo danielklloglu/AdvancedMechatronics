6d662ad1ba2312b330898ca35d49888be3154cd1
danielklloglu <danielklloglu@gmail.com>
2025-04-08 12:19:21 -0500

    Update hello_gpio_irq.c

--- hello_gpio_irq/hello_gpio_irq.c
+++ hello_gpio_irq/hello_gpio_irq.c
@@ -11,6 +11,33 @@
 #define GPIO_WATCH_PIN 2
 
 static char event_str[128];
+static volatile int ctr = 0;
+
+// Perform initialisation
+int pico_led_init(void) {
+    #if defined(PICO_DEFAULT_LED_PIN)
+        // A device like Pico that uses a GPIO for the LED will define PICO_DEFAULT_LED_PIN
+        // so we can use normal GPIO functionality to turn the led on and off
+        gpio_init(PICO_DEFAULT_LED_PIN);
+        gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+        return PICO_OK;
+    #elif defined(CYW43_WL_GPIO_LED_PIN)
+        // For Pico W devices we need to initialise the driver etc
+        return cyw43_arch_init();
+    #endif
+    }
+    
+    // Turn the led on or off
+    void pico_set_led(bool led_on) {
+    #if defined(PICO_DEFAULT_LED_PIN)
+        // Just set the GPIO on or off
+        gpio_put(PICO_DEFAULT_LED_PIN, led_on);
+    #elif defined(CYW43_WL_GPIO_LED_PIN)
+        // Ask the wifi "driver" to set the GPIO on or off
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led_on);
+    #endif
+}
+
 
 void gpio_event_string(char *buf, uint32_t events);
 
@@ -18,12 +45,25 @@ void gpio_callback(uint gpio, uint32_t events) {
     // Put the GPIO event(s) that just happened into event_str
     // so we can print it
     gpio_event_string(event_str, events);
-    printf("GPIO %d %s\n", gpio, event_str);
+    //printf("GPIO %d %s\n", gpio, event_str);
+    if (events & GPIO_IRQ_EDGE_RISE){
+        pico_set_led(true);
+        ctr++;
+        printf("Button Press Count: %d\n", ctr);
+    }
+    if (events & GPIO_IRQ_EDGE_FALL){
+        pico_set_led(false);
+    }
 }
 
 int main() {
     stdio_init_all();
 
+    int rc = pico_led_init();
+    hard_assert(rc == PICO_OK);
+
+    sleep_ms(3000);
+    
     printf("Hello GPIO IRQ\n");
     gpio_init(GPIO_WATCH_PIN);
     gpio_set_irq_enabled_with_callback(GPIO_WATCH_PIN, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
