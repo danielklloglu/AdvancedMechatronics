a45bf9162f6d733352f1fed2da016e727193981f
danielklloglu <danielklloglu@gmail.com>
2025-04-14 21:58:18 -0500

    hw4

--- hw4/hw4.c
+++ hw4/hw4.c
@@ -7,18 +7,30 @@
 // Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
 #define SPI_PORT spi0
 #define PIN_MISO 16
-#define PIN_CS   17
+#define PIN_CS   20
 #define PIN_SCK  18
 #define PIN_MOSI 19
 
+static inline void cs_select(uint cs_pin) {
+    asm volatile("nop \n nop \n nop"); // FIXME
+    gpio_put(cs_pin, 0);
+    asm volatile("nop \n nop \n nop"); // FIXME
+}
 
+static inline void cs_deselect(uint cs_pin) {
+    asm volatile("nop \n nop \n nop"); // FIXME
+    gpio_put(cs_pin, 1);
+    asm volatile("nop \n nop \n nop"); // FIXME
+}
+
+void writeDAC();
 
 int main()
 {
     stdio_init_all();
 
     // SPI initialisation. This example will use SPI at 1MHz.
-    spi_init(SPI_PORT, 1000*1000);
+    spi_init(SPI_PORT, 12000); // set to a 1000 to see easier
     gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
     gpio_set_function(PIN_CS,   GPIO_FUNC_SIO);
     gpio_set_function(PIN_SCK,  GPIO_FUNC_SPI);
@@ -32,5 +44,16 @@ int main()
     while (true) {
         printf("Hello, world!\n");
         sleep_ms(1000);
+        writeDAC();
     }
+}
+
+void writeDAC(){
+    uint8_t data[2];
+    int len = 2
+    data[0] = 0b10101010;
+    data[1] = 255;
+    cs_select(PIN_CS);
+    spi_write_blocking(SPI_PORT, data, len); // where data is a uint8_t array with length len
+    cs_deselect(PIN_CS);
 }
